c470700e55d38d3eb793d129f7df6ff523e928b9
Azul <mail@azulinho.com>
2014-12-15 12:51:11 +0000

    add missing tests, fix issue with defaults rule for authorized_keys!

--- vagrantbox/main_spec.rb
+++ vagrantbox/main_spec.rb
@@ -1,2 +1,30 @@
 require_relative 'spec_helper'
 
+
+describe file('/home/vagrant/.ssh') do
+  it { should be_directory }
+  it { should be_mode 700 }
+  it { should be_owned_by 'vagrant' }
+  it { should be_grouped_into 'vagrant' }
+end
+
+describe file('/home/vagrant/.ssh/id_rsa') do
+  it { should be_file }
+  it { should be_mode 600 }
+  it { should be_owned_by 'vagrant' }
+  it { should be_grouped_into 'vagrant' }
+end
+
+describe file('/home/vagrant/.ssh/id_rsa.pub') do
+  it { should be_file }
+  it { should be_mode 600 }
+  it { should be_owned_by 'vagrant' }
+  it { should be_grouped_into 'vagrant' }
+end
+
+describe file('/home/vagrant/.ssh/authorized_keys') do
+  it { should be_file }
+  it { should be_mode 600 }
+  it { should be_owned_by 'vagrant' }
+  it { should be_grouped_into 'vagrant' }
+end
